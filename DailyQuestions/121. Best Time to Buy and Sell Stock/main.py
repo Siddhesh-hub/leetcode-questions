c9d3c61d80ce1ba19eaa1482b81d7722dba51b5a
Siddhesh Pansare <61089801+Siddhesh-hub@users.noreply.github.com>
2026-03-06 11:43:21 +0530

    Implement maxProfit method using Kadane's algorithm

---
+++ DailyQuestions/121. Best Time to Buy and Sell Stock/main.py
@@ -0,0 +1,12 @@
+class Solution:
+    #Using kadanes algorithm
+    def maxProfit(self, prices: List[int]) -> int:
+        buy = prices[0]
+        profit = 0
+        n = len(prices)
+        for i in range(1, n):
+            if prices[i] < buy:
+                buy = prices[i]
+            elif prices[i] - buy > profit:
+                profit = prices[i] - buy
+        return profit
